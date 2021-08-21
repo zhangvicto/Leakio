33705c493335d36fb6f13e0d480e6518ea02e341
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2021-08-20 20:40:34 -0400

    Create app.ino

---
+++ software/app/app.ino
@@ -0,0 +1,57 @@
+//Sensor Pin
+const int sensorPin = A1;
+
+//Smoothing vars
+const int numReadings = 2;
+int readings[numReadings];
+int readIndex = 0;
+int total = 0;
+int average = 0;
+
+void setup() {
+  //Serial
+  Serial.begin(112500);
+
+  //Initializing all readings
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+}
+
+void loop() {
+  total = total - readings[readIndex];
+  readings[readIndex] = analogRead(sensorPin);
+  total = total + readings[readIndex];
+  readIndex = readIndex + 1;
+
+  if (readIndex >= numReadings) {
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+
+  int currentTime = 0;
+  int interval = 2000; //ms
+  int pastTime = 0;
+  int count = 0;
+
+  currentTime = millis();
+
+  if (currentTime - pastTime >= interval) {
+    if (average == 0) {
+      Serial.println("Leak detected!");
+      count++;
+    } else {
+      Serial.println("Normal");
+      count--;
+    }
+
+    if (count > 5) {
+      Serial.println("There is a leak, please seek help to prevent further damage.");
+    } else {
+      pastTime = currentTime;
+    }
+    
+  }
+}
