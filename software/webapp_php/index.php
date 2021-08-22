a4f763a767c3cda55f390983ab27c975d93544c9
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2021-08-22 01:43:50 -0400

    upload 3d model files

--- software/webapp_php/index.php
+++ software/webapp_php/index.php
@@ -1,109 +1,152 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <link rel="stylesheet" href="./style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-</head>
-
-<title>Leakio</title>
-
-<body>
-    <div class="block"></div>
-    <div class="container">
-        
-        <div class="title-text animate__animated animate__fadeInLeft">
-            Welcome back, <br>
-            <span class="name-text">Hackthe6ix</span>
-        </div>
-
-        <?php
-$servername = "192.168.2.57";
-$username = "root";
-$password = "victor";
-$dbname = "leakdb";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+<?php
+    /*
+     * Copyright 2020 Google LLC.
+     *
+     * Licensed under the Apache License, Version 2.0 (the "License");
+     * you may not use this file except in compliance with the License.
+     * You may obtain a copy of the License at
+     *
+     *     http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+     */
+    declare(strict_types=1);
+   
+    namespace Google\Cloud\Samples\CloudSQL\MySQL;
+    
+    use PDO;
+    use PDOException;
+    use RuntimeException;
+    
+    include 'content.html';
+    
+    class DBInitializer
+    {
+    
+        /**
+         *  @param $username string username of the database user
+         *  @param $password string password of the database user
+         *  @param $dbName string name of the target database
+         *  @param $dbHost string IP address or domain of the target database
+         *  @param $connConfig array driver-specific options for PDO
+         */
+        public static function initTcpDatabaseConnection(
+            string $username,
+            string $password,
+            string $dbName,
+            string $dbHost,
+            array $connConfig
+        ): PDO {
+            try {
+                # [START cloud_sql_mysql_pdo_create_tcp]
+                $username = 'root';
+                $password = 'victor';
+                $dbName = 'leakdb';
+                $dbHost = "192.168.2.57";
+    
+                // Connect using TCP
+                $dsn = sprintf('mysql:dbname=%s;host=%s', $dbName, $dbHost);
+    
+                // Connect to the database
+                $conn = new PDO($dsn, $username, $password, $connConfig);
+                # [END cloud_sql_mysql_pdo_create_tcp]
+            } catch (TypeError $e) {
+                throw new RuntimeException(
+                    sprintf(
+                        'Invalid or missing configuration! Make sure you have set ' .
+                        '$username, $password, $dbName, and $dbHost (for TCP mode) ' .
+                        'or $connectionName (for UNIX socket mode). ' .
+                        'The PHP error was %s',
+                        $e->getMessage()
+                    ),
+                    $e->getCode(),
+                    $e
+                );
+            } catch (PDOException $e) {
+                throw new RuntimeException(
+                    sprintf(
+                        'Could not connect to the Cloud SQL Database. Check that ' .
+                        'your username and password are correct, that the Cloud SQL ' .
+                        'proxy is running, and that the database exists and is ready ' .
+                        'for use. For more assistance, refer to %s. The PDO error was %s',
+                        'https://cloud.google.com/sql/docs/mysql/connect-external-app',
+                        $e->getMessage()
+                    ),
+                    $e->getCode(),
+                    $e
+                );
+            }
+    
+            return $conn;
+        }
+    
+        /**
+         *  @param $username string username of the database user
+         *  @param $password string password of the database user
+         *  @param $dbName string name of the target database
+         *  @param $connectionName string Cloud SQL instance name
+         *  @param $socketDir string Full path to unix socket
+         *  @param $conn_config array driver-specific options for PDO
+         */
+        public static function initUnixDatabaseConnection(
+            string $username,
+            string $password,
+            string $dbName,
+            string $connectionName,
+            string $socketDir,
+            array $conn_config
+        ): PDO {
+            try {
+                # [START cloud_sql_mysql_pdo_create_socket]
+                // $username = 'your_db_user';
+                // $password = 'yoursupersecretpassword';
+                // $dbName = 'your_db_name';
+                // $connectionName = getenv("CLOUD_SQL_CONNECTION_NAME");
+                // $socketDir = getenv('DB_SOCKET_DIR') ?: '/cloudsql';
+    
+                // Connect using UNIX sockets
+                $dsn = sprintf(
+                    'mysql:dbname=%s;unix_socket=%s/%s',
+                    $dbName,
+                    $socketDir,
+                    $connectionName
+                );
+    
+                // Connect to the database.
+                $conn = new PDO($dsn, $username, $password, $conn_config);
+                # [END cloud_sql_mysql_pdo_create_socket]
+            } catch (TypeError $e) {
+                throw new RuntimeException(
+                    sprintf(
+                        'Invalid or missing configuration! Make sure you have set ' .
+                        '$username, $password, $dbName, and $dbHost (for TCP mode) ' .
+                        'or $connectionName (for UNIX socket mode). ' .
+                        'The PHP error was %s',
+                        $e->getMessage()
+                    ),
+                    $e->getCode(),
+                    $e
+                );
+            } catch (PDOException $e) {
+                throw new RuntimeException(
+                    sprintf(
+                        'Could not connect to the Cloud SQL Database. Check that ' .
+                        'your username and password are correct, that the Cloud SQL ' .
+                        'proxy is running, and that the database exists and is ready ' .
+                        'for use. For more assistance, refer to %s. The PDO error was %s',
+                        'https://cloud.google.com/sql/docs/mysql/connect-external-app',
+                        $e->getMessage()
+                    ),
+                    $e->getCode(),
+                    $e
+                );
+            }
+    
+            return $conn;
+        }
     }
 
-    $sql = "SELECT id, firstname, lastname FROM MyGuests";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-    }
-    } else {
-    echo "0 results";
-    }
-    $conn->close();
-    ?>
-
-        <div class="bubble-wrap animate__animated animate__fadeInLeft animate__delay-1s">
-            <div class="text-bubble">
-                <div class="text-wrap">
-                    <div class="text">
-                        Status:<span id="replace"> No detected leaks</span> <br>
-                        <span style="font-size: 12px;font-weight: 400;">Sound & Water sensors have not detected any
-                            abnormal activity in past 24 hours.
-                        </span>
-                        <div class="button">
-                            <span
-                                style="font-size:12px;position:absolute; left:50%;top: 50%;transform: translate(-50%,-50%);width:100%;text-align: center;">
-                                View Devices
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="next animate__animated animate__fadeInLeft animate__delay-1s">
-            <span style="font-size: 12px;">Next Steps: Update Security</span>
-            <div class="button2"><span
-                    style="font-size:12px;position:absolute; left:50%;top: 50%;transform: translate(-50%,-50%);width:100%;text-align: center;">
-                    Update
-                </span></div>
-        </div>
-
-        <div class="graph animate__animated animate__fadeInLeft animate__delay-2s">
-            <span style="font-weight: 700;">Acoustic Status:</span>
-            <span style="color:green;">Good</span>
-            <img class="graph-img" id="graph" src="./img/graph_img.png">
-        </div>
-
-        <div class="menu-bar">
-            <li>
-                <div class="icon-container"><img class="icon" src="./img/search.png"></div>
-            </li>
-            <li>
-                <div class="icon-container"><img class="icon" src="./img/stats.png"></div>
-            </li>
-            <li>
-                <div class="icon-container active"><img class="icon icon-active" src="./img/home.png"></div>
-            </li>
-            <li>
-                <div class="icon-container"><img class="icon" src="./img/cam.png"></div>
-            </li>
-            <li>
-                <div class="icon-container"><img class="icon" src="./img/person.png"></div>
-            </li>
-        </div>
-    </div>
-
-    <script src="./script.js"></script>
-    <script>
-        if (CSS && 'paintWorklet' in CSS) CSS.paintWorklet.addModule('https://unpkg.com/smooth-corners')
-    </script>
-
-</body>
-
-</html>
