31d78c41aac9c4b7e325b2ebfaea84b0a7e5446e
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2021-08-21 21:49:24 -0400

    website frame complete

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-var connection = new ActiveXObject("ADODB.Connection") ;
+// var connection = new ActiveXObject("ADODB.Connection");
 
-var connectionstring="Data Source=<server>;Initial Catalog=<catalog>;User ID=<user>;Password=<password>;Provider=SQLOLEDB";
+// var connectionstring = "Data Source=<server>;Initial Catalog=<catalog>;User ID=<user>;Password=<password>;Provider=SQLOLEDB";
 
-connection.Open(connectionstring);
-var rs = new ActiveXObject("ADODB.Recordset");
+// connection.Open(connectionstring);
+// var rs = new ActiveXObject("ADODB.Recordset");
 
-rs.Open("SELECT * FROM table", connection);
-rs.MoveFirst
-while(!rs.eof)
-{
-   document.write(rs.fields(1));
-   rs.movenext;
-}
+// rs.Open("SELECT * FROM table", connection);
+// rs.MoveFirst
+// while (!rs.eof) {
+//     document.write(rs.fields(1));
+//     rs.movenext;
+// }
 
-rs.close;
-connection.close; 
+// rs.close;
+// connection.close;
+
+document.getElementById('graph').ondragstart = function() { return false; };
